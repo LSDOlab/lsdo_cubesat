fe64dd7e6a3f0f4a8ce70fd2220bd78f95c0a6c2
vgucsd <vgandari@eng.ucsd.edu>
2022-01-18 12:09:58 -0800

    update rw_speed

--- lsdo_cubesat/attitude/attitude_cadre_style.py
+++ lsdo_cubesat/attitude/attitude_cadre_style.py
@@ -5,87 +5,9 @@ from scipy.sparse import csc_matrix
 from csdl import Model
 import csdl
 
-from lsdo_cubesat.operations.rk4_op import RK4
+from lsdo_cubesat.operations.rw_speed import RWSpeed
 from lsdo_cubesat.utils.rot_seq import rot_seq
 from csdl.utils.get_bspline_mtx import get_bspline_mtx
-
-
-def skew(a, b, c):
-    return np.array([
-        [0, -c, b],
-        [c, 0, -a],
-        [-b, a, 0],
-    ])
-
-
-def skew_array(a):
-    return np.array([
-        [0, -a[2], a[1]],
-        [a[2], 0, -a[0]],
-        [-a[1], a[0], 0],
-    ])
-
-
-class RWSpeed(RK4):
-
-    def initialize(self):
-        super().initialize()
-        self.parameters.declare('num_times', types=int)
-        self.parameters.declare('rw_mmoi', types=np.ndarray)
-
-        self.parameters['external_vars'] = [
-            'body_torque',
-            'body_rates',
-        ]
-        self.parameters['init_state_var'] = 'initial_rw_speed'
-        self.parameters['state_var'] = 'rw_speed'
-
-    def define(self):
-        num_times = self.parameters['num_times']
-        rw_mmoi = self.parameters['rw_mmoi']
-        # external
-        self.add_input(
-            'body_torque',
-            shape=(3, num_times),
-        )
-        self.add_input(
-            'body_rates',
-            shape=(3, num_times),
-        )
-        # initial
-        self.add_input(
-            'initial_rw_speed',
-            val=0,
-            shape=3,
-        )
-        # integrated
-        self.add_output(
-            'rw_speed',
-            shape=(3, num_times),
-        )
-        self.dfdx = np.zeros((3, 6))
-        self.rw_mmoi = np.diag(rw_mmoi)
-        self.rw_mmoi_inv = np.diag(1 / rw_mmoi)
-        self.dfdx[:, :3] = -self.rw_mmoi_inv
-
-    def f_dot(self, external, state):
-        J = self.parameters['rw_mmoi']
-        body_torque = external[:3]
-        body_rates = external[3:6]
-        return -np.matmul(self.rw_mmoi_inv,
-                          (np.cross(body_rates, J * state) + body_torque))
-
-    # ODE wrt state variables
-    def df_dy(self, external, state):
-        body_rates = external[3:6]
-        return -np.matmul(self.rw_mmoi_inv,
-                          np.matmul(skew_array(body_rates), self.rw_mmoi))
-
-    # ODE wrt external inputs
-    def df_dx(self, external, state):
-        Jw = np.matmul(self.rw_mmoi, state)
-        self.dfdx[:, 3:] = np.matmul(self.rw_mmoi_inv, skew_array(Jw))
-        return self.dfdx
 
 
 class Attitude(Model):
@@ -372,4 +294,4 @@ if __name__ == "__main__":
             gravity_gradient=True,
         ))
     # sim.visualize_implementation()
-    sim.check_partials(compact_print=True, method='fd')
+    sim.check_partials(compact_print=True, method='cs')
